d9b690072182ad62e8f9c477fba5ad411395fe51
Lucas <Lucas@LucasVictor-pc>
2015-11-26 21:04:37 -0200

    commit com as pastas do bootstrap

--- t2-lpw-noite/projeto_principal/index.php
+++ t2-lpw-noite/projeto_principal/index.php
@@ -177,10 +177,10 @@ else
             
             <div class="col-md-3 col-sm-6 hero-feature">
                 <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
+                    <img src="images\embalagem1.png" alt="">
                     <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h3>Embalagem de Alimentos</h3>
+                        <p>Embalagem 100% biodegradável confeccionada com papel cartão.</p>
                         <p>
                             <a href="#" class="btn btn-primary">Trocar</a> 
                         </p>
