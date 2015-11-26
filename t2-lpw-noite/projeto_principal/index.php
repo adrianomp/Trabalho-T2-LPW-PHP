40782c6e654c986bda7357f1cd6eed720c48f677
Lucas <Lucas@LucasVictor-pc>
2015-11-26 21:15:45 -0200

    commit com as pastas do bootstrap versao 3

--- t2-lpw-noite/projeto_principal/index.php
+++ t2-lpw-noite/projeto_principal/index.php
@@ -163,10 +163,10 @@ else
 
             <div class="col-md-3 col-sm-6 hero-feature">
                 <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
+                    <img src="projeto_principal\images\coposdec1.jpg" alt="">
                     <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <h3>Copos Decorados</h3>
+                        <p>Copos de 90ml biodegradáveis utilizados para bebidas quentes.</p>
                         <p>
                             <a href="#" class="btn btn-primary">Trocar</a> 
                         </p>
