4a23e041eabad815121b6290d3ebb0e53d69106a
Adriano <Adriano@Adriano-PC>
2015-11-29 21:40:57 -0200

    adicionando menu no login e no cadastro, apagando arquivos inuteis

--- t2-lpw-noite/projeto_principal/cadastrar_usuario.php
+++ t2-lpw-noite/projeto_principal/cadastrar_usuario.php
@@ -1,102 +1,111 @@
-<head>
+<?php
+session_start();
+$logado = isset($_SESSION["usuario"]);
+?>
+<html>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Heroic Features - Start Bootstrap Template</title>
+        <title>Heroic Features - Start Bootstrap Template</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/heroic-features.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/heroic-features.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-</head>
-
-<form class="form-horizontal" method="post" action="cadastro.php">
-    <fieldset>
-
+    </head>
+    <body>
         <?php
-        if (isset($msgErroEmail)) {
-            ?>
-            <div class="alert alert-danger">
-                <center> <a class="close" data-dismiss="alert" href="#"></a><?= $msgErroEmail ?></center>
-            </div>
-        <?php } ?> 
-        <?php
-        if (isset($msgErroSenha)) {
-            ?>
-            <div class="alert alert-danger">
-                <center> <a class="close" data-dismiss="alert" href="#"></a><?= $msgErroSenha ?></center>
-            </div>
-        <?php } ?> 
-        <?php
-        if (isset($msgErroUsuario)) {
-            ?>
-            <div class="alert alert-danger">
-                <center> <a class="close" data-dismiss="alert" href="#"></a><?= $msgErroUsuario ?></center>
-            </div>
-        <?php } ?> 
+        require_once("menu_superior.php");
+        ?>
 
-        <!-- Form Name -->
-        <center><legend>Cadastro</legend></center>
+        <form class="form-horizontal" method="post" action="cadastro.php">
+            <fieldset>
 
-        <!-- Text input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="idNome">Nome</label>  
-            <div class="col-md-5">
-                <input id="idNome" name="nome" type="text" placeholder="Informe seu nome" class="form-control input-md" required="">
+                <?php
+                if (isset($msgErroEmail)) {
+                    ?>
+                    <div class="alert alert-danger">
+                        <center> <a class="close" data-dismiss="alert" href="#"></a><?= $msgErroEmail ?></center>
+                    </div>
+                <?php } ?> 
+                <?php
+                if (isset($msgErroSenha)) {
+                    ?>
+                    <div class="alert alert-danger">
+                        <center> <a class="close" data-dismiss="alert" href="#"></a><?= $msgErroSenha ?></center>
+                    </div>
+                <?php } ?> 
+                <?php
+                if (isset($msgErroUsuario)) {
+                    ?>
+                    <div class="alert alert-danger">
+                        <center> <a class="close" data-dismiss="alert" href="#"></a><?= $msgErroUsuario ?></center>
+                    </div>
+                <?php } ?> 
 
-            </div>
-        </div>
+                <!-- Form Name -->
+                <center><legend>Cadastro</legend></center>
 
-        <!-- Text input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="idDepto">E-mail</label>  
-            <div class="col-md-5">
-                <input id="idDepto" name="email" type="text" placeholder="Informe seu e-mail" class="form-control input-md" required="">
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="idNome">Nome</label>  
+                    <div class="col-md-5">
+                        <input id="idNome" name="nome" type="text" placeholder="Informe seu nome" class="form-control input-md" required="">
 
-            </div>
-        </div>
+                    </div>
+                </div>
 
-        <!-- Text input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="idUsuario">Login</label>  
-            <div class="col-md-5">
-                <input id="idUsuario" name="login" type="text" placeholder="Informe seu login" class="form-control input-md" required="">
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="idDepto">E-mail</label>  
+                    <div class="col-md-5">
+                        <input id="idDepto" name="email" type="text" placeholder="Informe seu e-mail" class="form-control input-md" required="">
 
-            </div>
-        </div>
+                    </div>
+                </div>
 
-        <!-- Password input-->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="idSenha">Senha</label>
-            <div class="col-md-5">
-                <input id="idSenha" name="senha" type="password" placeholder="Informe sua senha" class="form-control input-md" required="">
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="idUsuario">Login</label>  
+                    <div class="col-md-5">
+                        <input id="idUsuario" name="login" type="text" placeholder="Informe seu login" class="form-control input-md" required="">
 
-            </div>
-        </div>
+                    </div>
+                </div>
 
+                <!-- Password input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="idSenha">Senha</label>
+                    <div class="col-md-5">
+                        <input id="idSenha" name="senha" type="password" placeholder="Informe sua senha" class="form-control input-md" required="">
 
-        <!-- Button (Double) -->
-        <div class="form-group">
-            <label class="col-md-4 control-label" for="idConfirmar"></label>
-            <div class="col-md-8">
-                <button id="idConfirmar" type="submit" name="enviar" class="btn btn-primary">Confirmar</button>
-            </div>
-        </div>
-
-    </fieldset>
-</form>
+                    </div>
+                </div>
 
 
+                <!-- Button (Double) -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="idConfirmar"></label>
+                    <div class="col-md-8">
+                        <button id="idConfirmar" type="submit" name="enviar" class="btn btn-primary">Confirmar</button>
+                    </div>
+                </div>
+
+            </fieldset>
+        </form>
+    </body>
+</html>
--- t2-lpw-noite/projeto_principal/logar_usuario.php
+++ t2-lpw-noite/projeto_principal/logar_usuario.php
@@ -1,5 +1,9 @@
+<?php
+session_start();
+$logado = isset($_SESSION["usuario"]);
+?>
 <html>
-   <head>
+    <head>
 
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,59 +22,63 @@
     </head>
 
     <body>
-        
-        
-<div class="container" style="margin-top:30px">
-<div class="col-md-4 col-md-offset-4">
-    <div class="panel panel-default">
-  <div class="panel-heading"><h3 class="panel-title"><strong>Login </strong></h3>
-      
-  </div>
-  
-  <div class="panel-body">
-      <form role="form" method="post" action="login.php">
-         <?php
-            if (isset($msgErro)) {
-                ?>
-        <div class="alert alert-danger">
-       <a class="close" data-dismiss="alert" href="#"></a><?= $msgErro ?>
-            </div>
-                
-                <?php
-            }
-            ?>	
-   
-  <div style="margin-bottom: 12px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                        <input id="login-username" type="text" class="form-control" name="login" value="" placeholder="Informe seu login">                                        
-                                    </div>
-                                
+        <?php
+        require_once("menu_superior.php");
+        ?>
+
+
+        <div class="container" style="margin-top:30px">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h3 class="panel-title"><strong>Login </strong></h3>
+
+                    </div>
+
+                    <div class="panel-body">
+                        <form role="form" method="post" action="login.php">
+                            <?php
+                            if (isset($msgErro)) {
+                                ?>
+                                <div class="alert alert-danger">
+                                    <a class="close" data-dismiss="alert" href="#"></a><?= $msgErro ?>
+                                </div>
+
+                                <?php
+                            }
+                            ?>	
+
                             <div style="margin-bottom: 12px" class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                        <input id="login-password" type="password" class="form-control" name="senha" placeholder="Informe sua senha">
-                                    </div>
-                                    
-                                    
-                                    
-  <button type="submit" class="btn btn-success" name="enviar">Entrar</button>
-  
-  <hr style="margin-top:10px;margin-bottom:10px;" >
-  
-  <div class="form-group">
-                                    
-                                        <div style="font-size:85%">
-                                            Não tem uma conta? 
-                                            <a href="cadastrar_usuario.php" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-                                            Cadastre-se!
-                                        </a>
-                                        </div>
-                                    
-                                </div> 
-</form>
-  </div>
-</div>
-</div>
-</div>
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                <input id="login-username" type="text" class="form-control" name="login" value="" placeholder="Informe seu login">                                        
+                            </div>
+
+                            <div style="margin-bottom: 12px" class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input id="login-password" type="password" class="form-control" name="senha" placeholder="Informe sua senha">
+                            </div>
+
+
+
+                            <button type="submit" class="btn btn-success" name="enviar">Entrar</button>
+
+                            <hr style="margin-top:10px;margin-bottom:10px;" >
+
+                            <div class="form-group">
+
+                                <div style="font-size:85%">
+                                    Não tem uma conta? 
+                                    <a href="cadastrar_usuario.php" onClick="$('#loginbox').hide();
+                                            $('#signupbox').show()">
+                                        Cadastre-se!
+                                    </a>
+                                </div>
+
+                            </div> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 
 </html>
